61f4e012437247052ebe43917ff6eecffc087ccb
Pablo <pmontalvoster@gmail.com>
2024-09-24 18:41:09 +0100

    files

---
+++ src/main/java/com/pablodev9/novainvest/NovaInvestApplication.java
@@ -0,0 +1,13 @@
+package com.pablodev9.novainvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablodev9/novainvest/NovaInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablodev9.novainvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
